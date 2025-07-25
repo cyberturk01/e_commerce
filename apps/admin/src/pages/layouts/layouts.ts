e91d1d1c1145e98d64cfc8a2f827204c2377d0c1
Gokhan Yigit <yigitgokhan1@gmail.com>
2025-07-25 22:35:55 +0200

    Add user name from computed

--- apps/admin/src/pages/layouts/layouts.ts
+++ apps/admin/src/pages/layouts/layouts.ts
@@ -2,15 +2,22 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  inject,
   signal,
   ViewEncapsulation,
 } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { navigations } from '../../navigation';
 import { NavPipe } from '../../pipes/nav-pipe';
 import { FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import Breadcrumb from './breadcrumb/breadcrumb';
+import { Common } from '../../services/common';
 
 @Component({
   imports: [
@@ -30,10 +37,19 @@ export default class Layouts {
   readonly search = signal<string>('');
   readonly time = signal<Date | string>('');
   readonly navigations = computed(() => navigations);
+  readonly router = inject(Router);
+  readonly user = computed(() => this.#common.user()!);
+
+  readonly #common = inject(Common);
 
   constructor() {
     setInterval(() => {
       this.time.set(new Date());
     }, 1000);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigateByUrl('/login');
   }
 }
