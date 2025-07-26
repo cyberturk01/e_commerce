1b034bd1615b9a904375c0c31cde37c7e8021042
Gokhan Yigit <yigitgokhan1@gmail.com>
2025-07-27 00:17:10 +0200

    Add infinite scroll

--- apps/ui/src/pages/home/home.ts
+++ apps/ui/src/pages/home/home.ts
@@ -4,19 +4,38 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  effect,
   Pipe,
+  signal,
   ViewEncapsulation,
 } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, InfiniteScrollDirective],
   templateUrl: './home.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Home {
-  readonly result = httpResource<ProductModel[]>(() => 'api/products');
+  readonly limit = signal<number>(6);
+  readonly start = signal<number>(0);
+  readonly result = httpResource<ProductModel[]>(
+    () => 'api/products?_limit=' + this.limit() + '&_start=' + this.start()
+  );
   readonly data = computed(() => this.result.value() ?? []);
+  readonly dataSignal = signal<ProductModel[]>([]);
   readonly loading = computed(() => this.result.isLoading());
+
+  constructor() {
+    effect(() => {
+      this.dataSignal.update((prev) => [...prev, ...this.data()]);
+    });
+  }
+
+  onScroll() {
+    this.limit.update((prev) => prev + 6);
+    this.start.update((prev) => prev + 6);
+  }
 }
