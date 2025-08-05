10b642c39c1019a38f70c87c84db315ac1dcff9c
Gokhan Yigit <yigitgokhan1@gmail.com>
2025-08-06 00:52:44 +0200

    Add register and login pages

---
+++ apps/ui/src/pages/auth/login/login.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { UserModel } from '@shared/models/user.model';
+import { FlexiToastService } from 'flexi-toast';
+
+@Component({
+  imports: [FormsModule, RouterLink],
+  templateUrl: './login.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class Login {
+  readonly #http = inject(HttpClient);
+  readonly toast = inject(FlexiToastService);
+  readonly #router = inject(Router);
+  signIn(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    this.#http
+      .get<UserModel[]>(
+        'api/users?username=' +
+          form.value['userName'] +
+          '&password=' +
+          form.value['password']
+      )
+      .subscribe((res) => {
+        if (res.length === 0) {
+          this.toast.showToast(
+            'Error',
+            'Username or Password is incorrect',
+            'error'
+          );
+          return;
+        }
+        const user = res[0];
+        localStorage.setItem('res', JSON.stringify(user));
+        this.#router.navigateByUrl('/');
+      });
+  }
+}
