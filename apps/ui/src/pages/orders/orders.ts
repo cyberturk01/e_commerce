30b43c52bace86b1f8b6bda886ea26221d43b521
Gokhan Yigit <yigitgokhan1@gmail.com>
2025-08-11 16:57:37 +0200

    Finalize the project

---
+++ apps/ui/src/pages/orders/orders.ts
@@ -0,0 +1,46 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Common } from '../../services/common';
+import { httpResource } from '@angular/common/http';
+import { OrderModel } from '@shared/models/order.model';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+
+@Component({
+  imports: [DatePipe, CurrencyPipe],
+  templateUrl: './orders.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class Orders {
+  readonly limit = signal<number>(4);
+  readonly totalCount = signal<number>(16);
+  readonly waitingCount = signal<number>(3);
+  readonly completedCount = signal<number>(13);
+  readonly result = httpResource<OrderModel[]>(() => {
+    const endpoint = `api/orders?userId=${
+      this.#common.user()?.id
+    }&_limit=${this.limit()}`;
+    return endpoint;
+  });
+  readonly data = computed(() => this.result.value() ?? []);
+  readonly total = computed(() => {
+    let total = 0;
+    this.data().forEach((val) => {
+      val.baskets.forEach((d) => (total += d.price * d.quantity * 1.18));
+    });
+
+    return total;
+  });
+
+  readonly #common = inject(Common);
+
+  showMore() {
+    this.limit.update((prev) => prev + 4);
+  }
+}
